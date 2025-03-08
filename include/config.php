901d45b45631cd207244b8b848a45cb7660168ad
Mohamad Syafii <53438375+tutorajaib@users.noreply.github.com>
2025-03-08 12:30:28 +0700

    Add files via upload

---
+++ include/config.php
@@ -0,0 +1,5 @@
+<?php 
+if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");}; 
+$data['mikhmon'] = array ('1'=>'mikhmon<|<mikhmon','mikhmon>|>aWNlbA==');
+
+$data['CCR'] = array ('1'=>'CCR!172.16.99.242','CCR@|@noc','CCR#|#pqCV','CCR%bbb','CCR^uuu','CCR&Rp','CCR*10','CCR(1','CCR)','CCR=10','CCR@!@disable');
